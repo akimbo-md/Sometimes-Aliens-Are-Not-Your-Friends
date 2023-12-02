95655e92b2125a70ab70a7ebdae7e527d55bc32f
Hubrian5 <hub5@mymacewan.ca>
2023-12-01 18:37:47 -0700

    Transparency + z-ordering
    Can test by changing fragColor manually inside main and changing scene to the transparencyTest scene I made

--- src/main.js
+++ src/main.js
@@ -101,6 +101,7 @@ async function main() {
         uniform vec3 diffuseVal;
         uniform vec3 ambientVal;
         uniform vec3 specularVal;
+        uniform float alphaVal;
         uniform float nVal;
         uniform int samplerExists;
         uniform sampler2D uTexture;
@@ -260,6 +261,24 @@ function drawScene(gl, deltaTime, state) {
     sorted.map((object) => {
         gl.useProgram(object.programInfo.program);
         {
+            if (object.material.alpha < 1.0) {
+                // DONE turn off depth masking
+                // enable blending and specify blending function 
+                gl.depthMask(false);  // turn off depth masking
+                gl.enable(gl.BLEND);
+                gl.blendFunc(gl.ONE_MINUS_CONSTANT_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+                //gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+                
+            }
+            else {
+                // DONE disable blending 
+                // enable depth masking and z-buffering
+                // specify depth function
+                gl.depthMask(true);  // Added to enable depth masking
+                gl.disable(gl.BLEND);
+                gl.enable(gl.DEPTH_TEST);  // Added to enable depth testing
+                gl.depthFunc(gl.LESS);  // Added to specify depth function
+            }
             // Projection Matrix ....
             let projectionMatrix = mat4.create();
             let fovy = 90.0 * Math.PI / 180.0; // Vertical field of view in radians
@@ -316,6 +335,7 @@ function drawScene(gl, deltaTime, state) {
             gl.uniform3fv(object.programInfo.uniformLocations.ambientVal, object.material.ambient);
             gl.uniform3fv(object.programInfo.uniformLocations.specularVal, object.material.specular);
             gl.uniform1f(object.programInfo.uniformLocations.nVal, object.material.n);
+            //gl.uniform1f(object.programInfo.uniformLocations.alphaValue, object.material.alpha); //alpha is buggy for now
 
             // Lights
             // let mainLight = state.pointLights[0];
